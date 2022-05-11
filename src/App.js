85745dd5a243bfb4aeb21e90ae5372edb79c0da9
Aaron Hubber <aaronhubber@gmail.com>
2022-05-11 12:54:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState (1);
+  const [answer, setAnswer] = useState();
+  const [phrase, setPhrase] = useState('')
+
+  useEffect(()=>{
+    if (number % 3 === 0 && number % 5 === 0) {
+      setAnswer("fizzbuzz")
+    } else if (number % 3 === 0) {
+      setAnswer("fizz")
+    } else if (number % 5 === 0) {
+      setAnswer("buzz")
+    } else {
+      setAnswer(number);
+    }
+},[number])
+
+  useEffect(()=>{
+    if (number ===19)
+    setPhrase ("fuck!!")
+      else{
+        setPhrase("")
+
+      }
+  }, [number])
+
+
+  const handleClick = () =>{
+    setNumber(number+1)
+  }
+
+  const handleChange=(event)=>{
+    setPhrase(event.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button value={number} onClick={handleClick}>FizzyBuzzyButton</button>
+    <h2>The results are: {answer} </h2>
+    <h2>!!!!{phrase}!!!</h2>
+    {/* <form>
+    <input type="text" value={setPhrase} onChange={handleChange}>Test</input>
+    </form> */}
+    </>
+    
   );
 }
 
 export default App;
+
+
